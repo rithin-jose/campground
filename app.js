0699d1c09777a38901d2cc4b9535f587f8ca0467
unknown <rithinja@gmail.com>
2020-05-14 15:41:44 +0530

    Data Persistance update and database created

--- app.js
+++ app.js
@@ -1,43 +1,93 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-
-var campgrounds = [
-    {name:"test", image:"https://pixabay.com/get/54e5dd424856ae14f1dc84609620367d1c3ed9e04e5074417d287fd39645c7_340.jpg"},
-    {name:"test2", image:"https://pixabay.com/get/57e0d6424954ac14f1dc84609620367d1c3ed9e04e5074417d287fd39645c7_340.jpg"},
-    {name:"test3", image:"https://pixabay.com/get/5ee1dc404c52b10ff3d8992ccf2934771438dbf852547941722b7cd59f4f_340.jpg"},
-    {name:"test", image:"https://pixabay.com/get/54e5dd424856ae14f1dc84609620367d1c3ed9e04e5074417d287fd39645c7_340.jpg"},
-    {name:"test2", image:"https://pixabay.com/get/57e0d6424954ac14f1dc84609620367d1c3ed9e04e5074417d287fd39645c7_340.jpg"},
-    {name:"test3", image:"https://pixabay.com/get/5ee1dc404c52b10ff3d8992ccf2934771438dbf852547941722b7cd59f4f_340.jpg"},
-    {name:"test", image:"https://pixabay.com/get/54e5dd424856ae14f1dc84609620367d1c3ed9e04e5074417d287fd39645c7_340.jpg"},
-    {name:"test2", image:"https://pixabay.com/get/57e0d6424954ac14f1dc84609620367d1c3ed9e04e5074417d287fd39645c7_340.jpg"},
-    {name:"test3", image:"https://pixabay.com/get/5ee1dc404c52b10ff3d8992ccf2934771438dbf852547941722b7cd59f4f_340.jpg"}
-];
+var mongoose = require('mongoose');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine','ejs');
 
+mongoose.connect("mongodb://localhost/campground",{useNewUrlParser: true, useUnifiedTopology: true});
+
+//Schema setup
+var campgroundSchema = new mongoose.Schema({
+    name : String,
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//     {
+//         name:"test",
+//         image:"https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum voluptas et sequi quasi odio non facere nam beatae impedit, veritatis commodi ea nemo assumenda possimus, atque consequatur. Labore, ut odit."
+//     },function(err, campground){
+//         if(err){
+//             console.log("Error");
+//             console.log(err);
+//         }
+//         else{
+//             console.log("New Campground");
+//             console.log(campground);
+//         }
+// });
+
 app.get("/",(req,res) => {
     res.render("homepage");
 });
 
+//INDEX - show all campgrounds
 app.get("/campgrounds",(req,res) => {
-    res.render("campgrounds",{campgrounds:campgrounds});
+    Campground.find({},function(err,allCampgrounds){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("index",{campgrounds:allCampgrounds});
+        }
+    });
+    // res.render("campgrounds",{campgrounds:campgrounds});
 });
 
+//CREATE - add new campgrounds to DB
 app.post("/campgrounds",(req,res) => {
     //adding to array
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name:name,image:image};
-    campgrounds.push(newCampground);
-    //redirecting the page
-    res.redirect("/campgrounds");
+    var description = req.body.description;
+    var newCampground = {name:name,image:image,description:description};
+
+    Campground.create(newCampground,function(err, newlyCreated){
+        if(err){
+            console.log(err);            
+        }
+        else{
+            //redirect to campgrounds
+            res.redirect("/campgrounds");
+            console.log("created succesfully");
+            
+        }
+    });
 });
 
+//New - Show form to createnew campground
 app.get("/campgrounds/new",(req,res) => {
     res.render("new.ejs")
+});
+
+//Show - shows info about one campground
+app.get("/campgrounds/:id",function(req,res){
+    //find ground with the id and render show teplate with the details
+    Campground.findById(req.params.id,function(err,foundCampground){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("show",{campground:foundCampground});
+        }
+    });
 });
 
 app.get("*",(req,res) => {
