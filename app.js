8b0b901e4e39e8b98f6e5c5c452425eae52a894d
unknown <rithinja@gmail.com>
2020-05-23 17:58:02 +0530

    Nested routes created for comments

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 
 var Campground = require('./models/campground');
+var Comment = require('./models/comment');
 var seedDB = require('./seeds');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -24,7 +25,7 @@ app.get("/campgrounds",(req,res) => {
             console.log(err);
         }
         else{
-            res.render("index",{campgrounds:allCampgrounds});
+            res.render("campgrounds/index",{campgrounds:allCampgrounds});
         }
     });
     // res.render("campgrounds",{campgrounds:campgrounds});
@@ -53,7 +54,7 @@ app.post("/campgrounds",(req,res) => {
 
 //New - Show form to createnew campground
 app.get("/campgrounds/new",(req,res) => {
-    res.render("new.ejs")
+    res.render("campgrounds/new.ejs")
 });
 
 //Show - shows info about one campground
@@ -66,10 +67,50 @@ app.get("/campgrounds/:id",function(req,res){
         else{
             console.log("found");
             
-            res.render("show",{campground:foundCampground});
+            res.render("campgrounds/show",{campground:foundCampground});
         }
     });
 });
+
+// *******************************************************************************
+//                          Comments routes
+// *******************************************************************************
+
+app.get("/campgrounds/:id/comments/new",function(req,res){
+    Campground.findById(req.params.id,function(err,campground){
+        if(err){
+            console.log(err);            
+        }
+        else{
+            res.render("comments/new",{campground: campground});
+        }
+    });
+});
+app.post("/campgrounds/:id/comments",function(req,res){
+    //lookup campground using id
+    Campground.findById(req.params.id,function(err,campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds")
+        }
+        else{
+            //create new comment
+            Comment.create(req.body.comment,function(err,comment){
+                if(err){
+                    console.log(err); 
+                }
+                else{
+                    //connect new comment to campground
+                    campground.comments.push(comment);
+                    campground.save();
+                    //redirect campground show page
+                    res.redirect("/campgrounds/"+campground._id);
+                }
+            });
+        }
+    });
+});
+
 
 app.get("*",(req,res) => {
     res.send("404 Page not found");
